812ddb355a361308aca97a4e8e00eed9f1088f52
chenzhouxu <710916361@qq.com>
2021-01-07 12:01:30 +0800

    ts+react体验

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './TodoList.css';
+
+interface TodoListProps {
+  items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
+}
+
+const TodoList: React.FC<TodoListProps> = props => {
+  const { onDeleteTodo } = props;
+  return (
+    <ul>
+      {props.items.map(todo => (
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={e => onDeleteTodo(todo.id)}>DELETE</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
